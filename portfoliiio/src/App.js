abe228b6ef3d75832dbc8cf1f5dd10e8d16bfd23
Haithem Ghattas <haithemghattas.spectrum@gmail.com>
2022-07-30 18:37:38 +0100

    navBar + some styling

---
+++ portfoliiio/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
